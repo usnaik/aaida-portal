147ac5eca9ffda6a2ae17979e7fdce8d3d87d84c
usnaik <upendra_naik@yahoo.com>
2020-08-21 13:22:41 +0530

    Content updated

---
+++ src/js/test.js
@@ -0,0 +1,42 @@
+//update this with your $form selector
+var form_id = "jquery_form";
+
+var data = {
+  "access_token": "5korz41k8ep5e3pk8pati9t9"
+};
+
+function onSuccess() {
+  // remove this to avoid redirect
+  window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+}
+
+function onError(error) {
+  // remove this to avoid redirect
+  window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+}
+
+var sendButton = $("#" + form_id + " [name='send']");
+
+function send() {
+  sendButton.val('Sending…');
+  sendButton.prop('disabled', true);
+
+  var subject = $("#" + form_id + " [name='subject']").val();
+  var message = $("#" + form_id + " [name='text']").val();
+  data['subject'] = subject;
+  data['text'] = message;
+
+  $.post('https://postmail.invotes.com/send',
+    data,
+    onSuccess
+  ).fail(onError);
+
+  return false;
+}
+
+sendButton.on('click', send);
+
+var $form = $("#" + form_id);
+$form.submit(function (event) {
+  event.preventDefault();
+});
